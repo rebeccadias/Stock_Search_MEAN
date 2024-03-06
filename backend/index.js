6e3133664f7d27c0643648b93a2527062cc53fa6
Rebecca Dias <rebeccadias2510@gmail.com>
2024-03-06 00:40:46 -0800

    test

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,12 +9,18 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://admin:12345@rebecca.gwrn5bi.mongodb.net/?retryWrites=true&w=majority&appName=Rebecca', { 
-}).then(() => console.log('Connected to MongoDB Atlas'))
-.catch(err => console.error('Could not connect to MongoDB Atlas', err));
+mongoose
+  .connect(
+    "mongodb+srv://admin:12345@rebecca.gwrn5bi.mongodb.net/?retryWrites=true&w=majority&appName=Rebecca",
+    {}
+  )
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) => console.error("Could not connect to MongoDB Atlas", err));
 
-app.get('/', (req, res) => {
-    res.send('Hello, Express!');
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
 });
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+
+// Path: backend/models/Post.js
