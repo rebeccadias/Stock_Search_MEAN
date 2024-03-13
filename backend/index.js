ab07f6230a74a524b314dd396bfe2dd24f3fe579
Rebecca Dias <rebeccadias2510@gmail.com>
2024-03-13 13:00:57 -0400

    git pull issue commit

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const axios = require('axios');
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const FINNHUB_API_KEY = "cms30mhr01qlk9b10me0cms30mhr01qlk9b10meg";
 
 app.use(cors());
 app.use(express.json());
@@ -19,6 +21,40 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello, Express!");
+});
+
+app.get("/search", async (req, res) => {
+  const query = req.query.q;
+  const url = `https://finnhub.io/api/v1/search?q=${query}&token=cms30mhr01qlk9b10me0cms30mhr01qlk9b10meg`;
+
+  try {
+    const response = await axios.get(url);
+    const filteredData = response.data.result.filter(item => item.type === 'Common Stock' && !item.symbol.includes('.'));
+    res.json(filteredData);
+  } catch (error) {
+    console.error("Error fetching stock data", error);
+    res.status(500).send('Error fetching stock data');
+  }
+});
+
+app.get("/api/stock/profile", async (req, res) => {
+  const { symbol } = req.query;
+  try {
+    const response = await axios.get(`https://finnhub.io/api/v1/stock/profile2?symbol=${symbol}&token=${FINNHUB_API_KEY}`);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).send('Error fetching stock profile');
+  }
+});
+
+app.get("/api/stock/quote", async (req, res) => {
+  const { symbol } = req.query;
+  try {
+    const response = await axios.get(`https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${FINNHUB_API_KEY}`);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).send('Error fetching stock quote');
+  }
 });
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
