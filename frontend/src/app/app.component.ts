7de41b9adaeaeb2da8763226521025381f94291a
Rebecca Dias <rebeccadias2510@gmail.com>
2024-03-20 01:27:16 -0400

    fixed navbar issues

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+  isActiveSearch: boolean = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Check if the current URL is either /home or matches /search/something
+        this.isActiveSearch =
+          event.urlAfterRedirects === '/search/home' ||
+          event.urlAfterRedirects.startsWith('/search/');
+      }
+    });
+  }
 }
